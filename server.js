f08da12e54c7020016784e281eb601b499c1c6cc
luomanss <sami.luomansuu@gmail.com>
2017-04-06 23:54:08 +0300

    added index route

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// add static files
+app.use(express.static(path.resolve(__dirname, './public')));
 
 app.get('/:timestamp', (req, res) => {
   const ret = { unix: null, natural: null };
